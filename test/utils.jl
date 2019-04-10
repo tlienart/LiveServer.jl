673fa647f65d17bde03d87679cdd6f8d275464bc
Thibaut Lienart <tlienart@me.com>
2019-04-10 13:01:30 +1000

    version check

--- test/utils.jl
+++ test/utils.jl
@@ -19,7 +19,7 @@
     # NOTE: these tests work perfectly well on Julia 1.x but for some mysterious reason they
     # do not pass on Julia 1.0 Travis. Given that I can't be bothered to figure out why, I just
     # add a version check here.
-    if VERSION >= 1.1
+    if VERSION >= v"1.1"
         sleep(0.1)
         # after the first pass, `makejl` should have been called once (first pass)
         @test parse(Int, read("counterfile", String)) == 1
